c262d53887dc244feedd705bfb489eadc9f38b87
Abo89 <alberti1989@gmail.com>
2024-12-15 14:42:16 +0100

    new driver DimmerSwitch_v1.0

---
+++ drivers/dimmerswitch_v10/device.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const VIMARZigBeeLightDevice = require('../../lib/VIMARZigBeeLightDevice');
+
+class dimmable_led_strip extends VIMARZigBeeLightDevice {}
+
+module.exports = dimmable_led_strip;
