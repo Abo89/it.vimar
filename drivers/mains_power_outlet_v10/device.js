144d3ae3dc149939df79b4b07386a9df36ec55f8
Abo89 <alberti1989@gmail.com>
2026-03-06 17:04:51 +0100

    update mains_power_outlet_v10 device.js

--- drivers/mains_power_outlet_v10/device.js
+++ drivers/mains_power_outlet_v10/device.js
@@ -1,27 +1,58 @@
 'use strict';
 
 const { ZigBeeDevice } = require('homey-zigbeedriver');
-const { CLUSTER} = require('zigbee-clusters');
+const { CLUSTER } = require('zigbee-clusters');
 
-class Mains_Power_Outlet_v1_0 extends ZigBeeDevice {
+// Uncomment to enable debug logging:
+// const { debug } = require('zigbee-clusters');
+// debug(true);
+
+class MainsPowerOutlet extends ZigBeeDevice {
 
   async onNodeInit({ zclNode }) {
-    //this.enableDebug(); // only for debugging purposes
-    //this.printNode(); // only for debugging purposes
+    await super.onNodeInit({ zclNode });
+
+    // Uncomment to enable debug logging:
+    // this.enableDebug();
+    // this.printNode();
+
     this.registerCapability('onoff', CLUSTER.ON_OFF);
-        
-    this.registerCapability('measure_power', CLUSTER.ELECTRICAL_MEASUREMENT, {        
-        reportParser(value) {            
-            if (value < 0) return null;
-            return value;            
-        },
-        getOpts: {
-          getOnStart: true,
-          pollInterval: 10000
-        }
+
+    // Read the device's power multiplier/divisor on init so we can scale
+    // raw acPower values (reported in 10mW units) to Watts correctly.
+    const windowCoveringEndpoint = this.getClusterEndpoint(CLUSTER.ELECTRICAL_MEASUREMENT);
+    if (windowCoveringEndpoint !== null) {
+      const { acPowerMultiplier, acPowerDivisor } = await zclNode
+        .endpoints[windowCoveringEndpoint].clusters.electricalMeasurement
+        .readAttributes(['acPowerMultiplier', 'acPowerDivisor'])
+        .catch((err) => {
+          this.error('Failed to read acPowerMultiplier/acPowerDivisor, falling back to defaults', err);
+          return { acPowerMultiplier: 1, acPowerDivisor: 10 };
+        });
+
+      this._powerMultiplier = acPowerMultiplier ?? 1;
+      this._powerDivisor = acPowerDivisor ?? 10;
+    } else {
+      this._powerMultiplier = 1;
+      this._powerDivisor = 10;
+    }
+
+    this.registerCapability('measure_power', CLUSTER.ELECTRICAL_MEASUREMENT, {
+      reportParser: (value) => {
+        if (value < 0) return null;
+        return (value * this._powerMultiplier) / this._powerDivisor;
+      },
+      getOpts: {
+        getOnStart: true,
+        pollInterval: 10000,
+      },
     });
-  }  
+  }
 
-};
+  onDeleted() {
+    this.log('MainsPowerOutlet removed');
+  }
 
-module.exports = Mains_Power_Outlet_v1_0;
+}
+
+module.exports = MainsPowerOutlet;
